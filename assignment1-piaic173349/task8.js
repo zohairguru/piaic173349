7b2abe4c80920c0fc61710ff295bbce42c5a01c8
zohairguru <diasolarpower@gmail.com>
2023-01-27 18:38:56 +0500

    Add task8 with js file

---
+++ assignment1-piaic173349/task8.js
@@ -0,0 +1,9 @@
+var num1 = prompt("Enter a Number");
+
+var num2 = prompt("Enter second number");
+inputNum1 = parseInt(num1);
+inputNum2 = parseInt(num2);
+var sum = inputNum1 + inputNum2;
+var total = parseInt(sum);
+
+document.write("Sum of " + num1 + " and " + num2 + " is " + total);
