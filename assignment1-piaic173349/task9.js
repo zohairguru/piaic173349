1010191caae3561839eddff61fe782a279c13cca
zohairguru <diasolarpower@gmail.com>
2023-01-27 18:46:12 +0500

    add task9 with js file

---
+++ assignment1-piaic173349/task9.js
@@ -0,0 +1,34 @@
+var num1 = prompt("Enter a Number");
+
+var num2 = prompt("Enter second number");
+inputNum1 = parseInt(num1);
+inputNum2 = parseInt(num2);
+var sum = inputNum1 - inputNum2;
+var total = parseInt(sum);
+
+document.write("Subtration of " + num1 + " and " + num2 + " is " + total,"<br>");
+
+
+
+
+inputNum1 = parseInt(num1);
+inputNum2 = parseInt(num2);
+var sum = inputNum1 * inputNum2;
+var total = parseInt(sum);
+
+document.write("Multiplication " + num1 + " and " + num2 + " is " + total,"<br>");
+
+inputNum1 = parseInt(num1);
+inputNum2 = parseInt(num2);
+var sum = inputNum1 / inputNum2;
+var total = parseInt(sum);
+
+document.write("Division " + num1 + " and " + num2 + " is " + total, "<br>");
+
+
+inputNum1 = parseInt(num1);
+inputNum2 = parseInt(num2);
+var sum = inputNum1 % inputNum2;
+var total = parseInt(sum);
+
+document.write("Modulus " + num1 + " and " + num2 + " is " + total,"<br>");
