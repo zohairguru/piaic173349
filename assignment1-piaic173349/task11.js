30138d45b997b67baa7ef4a489e763ba5fa9a349
zohairguru <diasolarpower@gmail.com>
2023-01-30 10:44:51 +0500

    add task 11 temperature convertion

---
+++ assignment1-piaic173349/task11.js
@@ -0,0 +1,16 @@
+function convert() {
+    var c = document.getElementById('data').value;
+    //alert(c);
+    var f = (c * 1.8) + 32;
+    //alert(f);
+    document.getElementById('result').innerHTML = `${c} Celsius is  ${f} Fahrenheit`;
+}
+
+
+function celsius() {
+    var f = document.getElementById('tem').value;
+    alert(f);
+    var c = (f - 32) * 5 / 9;
+    alert(c);
+    document.getElementById('convertion').innerHTML = `${f} Fahrenheit is  ${c} Celius`;
+}
