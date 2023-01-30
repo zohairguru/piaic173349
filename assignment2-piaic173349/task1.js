1404c8d8248083aec5168e8b064aa7b1ab28462b
zohairguru <diasolarpower@gmail.com>
2023-01-30 15:35:29 +0500

    assigment2 task1 with js file

---
+++ assignment2-piaic173349/task1.js
@@ -0,0 +1,10 @@
+var msg = prompt("City Name");
+
+
+
+if(msg == "karachi") {
+    alert("Welcome to city of lights",msg);
+} else {
+    document.write("Please enter only city ", '<strong>',"Karachi",'</strong>' );
+}
+
