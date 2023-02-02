21351fd31a7077d06013b26566e3762e1fb8c56e
zohairguru <diasolarpower@gmail.com>
2023-02-02 18:35:40 +0500

    add assignmrnt 3 task 2 score and percentage with js

---
+++ assignment3-piaic173349/task2.js
@@ -0,0 +1,27 @@
+var students = ["Michael", "Jhon", "Tony"];
+
+
+var score = [320, 230, 480];
+
+
+var michael = score[0] / 500 * 100;
+
+var john = score[1] / 500 * 100;
+
+var tony = score[2] / 500 * 100;
+
+
+document.write("<h3>","Score of Michael is ",score[0],". Percentage : ",michael,"%","</h3>");
+
+document.write("<h3>","Score of John is ",score[1],". Percentage : ",john,"%","</h3>");
+
+
+document.write("<h3>","Score of Tony is ",score[2],". Percentage : ",tony,"%","</h3>");
+
+
+
+// alert(michael);
+
+// alert(john);
+
+// alert(tony);
