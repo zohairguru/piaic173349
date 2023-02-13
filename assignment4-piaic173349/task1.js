37e9536e31c7731ab9d307126da50c5f83fb0562
zohairguru <diasolarpower@gmail.com>
2023-02-13 16:57:31 +0500

    add assignment 4 task 1

---
+++ assignment4-piaic173349/task1.js
@@ -0,0 +1,8 @@
+ function replaceHyderWithIslam() {
+     var city = "Hyderabad";
+    //  document.write(city);
+            var newCity = city.replace("Hyder", "Islam");
+            document.getElementById("city").innerHTML = newCity;
+}
+        
+replaceHyderWithIslam();
